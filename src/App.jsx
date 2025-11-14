10142f78ac86ac166553961f448c8534a63b99c7
igk190 <graciakleijnen@gmail.com>
2025-11-14 17:43:55 +0000

    Clean up App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,45 @@ import Contact from './components/Contact';
 
 import './App.css'
 
-
 function App() {
-  const [count, setCount] = useState(0)
+
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const storedUserId = localStorage.getItem('userId');
+
+    if (token && storedUserId) {
+      setIsLoggedIn(true);
+      setUserId(storedUserId);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    navigate('/');
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Navi isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
+    
+    <main>
+      <Container classname='pt-5'>
+        <Routes>
+          <Route path='/' element={<Home isLoggedIn={ isLoggedIn } />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Container>
+    </main>
+
     </>
   )
 }
