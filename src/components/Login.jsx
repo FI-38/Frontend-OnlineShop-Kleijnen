8226997f6033119f62188a1bddfaa6f8d55b77b0
igk190 <graciakleijnen@gmail.com>
2025-11-17 17:11:50 +0000

    Form for Login.jsx.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
-function Login() {
+
+function Login({isLoggedIn}) {
+  
+  
   return (
-    <div>
-      <h1>Login</h1>
-      <p>This is just boilerplate so it renders safely.</p>
+    <div className='mt-4'>
+      <h3>{isLoggedIn ? 'Welcome back!' : 'Log in'}</h3>
+      {!isLoggedIn && (<Form>
+        <Form.Group className='mb-2'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control type='text' name='username' placeholder='maximuster' required />
+        </Form.Group>
+        <Form.Group className='mb-3'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' name='password' placeholder='yourpassword123' required/>
+        </Form.Group>
+        <Button variant='primary' type='submit'>Log in</Button>
+
+
+      </Form>)}
     </div>
   );
 }
 
 export default Login;
+
+
+// todos 
+// 1.  success message 
