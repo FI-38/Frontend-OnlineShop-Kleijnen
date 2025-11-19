53cdf3d93a79b5fe16fd95fa98ea9873b5a2d679
igk190 <graciakleijnen@gmail.com>
2025-11-19 14:27:25 +0000

    Added registration form.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
 
 function Register() {
   return (
-    <div>
+    <div className='mt-4'>
       <h1>Register</h1>
-      <p>This is just boilerplate so it renders safely.</p>
+      <Form>
+        <Form.Group className='mb-3'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control type='text' name='name' placeholder="Maxine" required />
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control type='text' name='username' placeholder="maxine126ch" required />
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control type='email' name='email' placeholder="maxine@mail.com" required />
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' name='password' placeholder="Choose password (min. 8 characters)" required minLength={8} />
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Repeat password</Form.Label>
+          <Form.Control type='password' name='confirmPassword' placeholder="Re-enter password" required />
+        </Form.Group>
+
+        <Button variant='primary' type='submit'>Register</Button>
+      </Form>
+    
     </div>
   );
 }
