815746bdb0425d6b1d3eda28cab3dc281de44d98
igk190 <graciakleijnen@gmail.com>
2025-11-20 15:08:47 +0000

    Add button to Dashboard.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Button } from "react-bootstrap"; 
+import { Link } from "react-router-dom";
+
+
 
 function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>This is just boilerplate so it renders safely.</p>
+      <p>No orders here yet. Find your next kicks</p>
+        <Button as={Link} to="/products" variant="primary">Find your next kicks</Button>
     </div>
   );
 }
