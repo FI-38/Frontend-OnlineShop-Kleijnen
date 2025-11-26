0133db37c6ac588702a6b78f30b8d93ff84c7455
igk190 <graciakleijnen@gmail.com>
2025-11-26 16:07:20 +0000

    ProductDetail page.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,12 +4,11 @@ import { Link } from "react-router-dom";
 
 
 function ProductCard({ title, description, price, image, productID}) {
-  const imageURL = image 
-  ? `${import.meta.env.VITE_API_SERVER_URL}/api/uploads/${image}` 
-  : "placeholder.jpg"; // fallback if no image
+  const imageURL =`${import.meta.env.VITE_API_SERVER_URL}/api/uploads/${image}` ;
 
   console.log('image prop:', image);
-  const shortDesc = description.slice(0, 60);
+  const shortDesc = description.slice(0, 50);
+  const shortDescWithDots = shortDesc.concat('...');
 
   return (
     <Link to={`/products/${productID}`} >
@@ -17,7 +16,7 @@ function ProductCard({ title, description, price, image, productID}) {
         <Card.Img variant="top" src={imageURL} />
         <Card.Body>
           <Card.Title>{title} - €{price} {productID}</Card.Title>
-          <Card.Text>{shortDesc} </Card.Text>
+          <Card.Text>{shortDescWithDots} </Card.Text>
         </Card.Body>
       </Card>
      </Link>
--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function ProductDetail () {
-    return (
-        <h1>Wow i will turn into product detial page.</h1>
-    )
+function ProductDetail() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const res = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/api/products/${id}`);
+        if (!res.ok) {
+            throw new Error("Failed to fetch product");
+        } 
+        const data = await res.json();
+        setProduct(data[0]); 
+      } catch (err) {
+        console.error("Error fetching product:", err);
+      }
+    };
+
+    fetchProduct();
+  }, []);
+
+  if (!product) return <>Loading...</>;
+
+  return (
+    <div>
+      <h1>{product.product_name}</h1>
+      <p>{product.product_description}</p>
+      <p>€ {product.product_price}</p>
+      <img src={`${import.meta.env.VITE_API_SERVER_URL}/api/uploads/${product.product_img_path}`} />
+    </div>
+  );
 }
 
 export default ProductDetail;
